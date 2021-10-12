082c071c1d8b4c8e0f7b86f41361317c46e591b5
ziedlassoued <zied.lassoued@outlook.de>
2021-10-12 12:14:02 +0200

    show text if answer is correct or incorect

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ myHeading.textContent = "are you a robot?";
 const myQuestion = document.querySelector("p");
 myQuestion.textContent = "question two";
 
-document.querySelector(".yes").onclick = function () {
+/*document.querySelector(".yes").onclick = function () {
   if (correctAnswer === true) {
     alert(" Correct");
   } else {
@@ -18,5 +18,22 @@ document.querySelector(".no").onclick = function () {
     alert(" Flase");
   } else {
     alert("Correct");
-  }
+  }*/
+
+const AnswerYes = document.querySelector(".yes");
+AnswerYes.onclick = function () {
+  const newP = document.createElement("p");
+  const newContent = document.createTextNode("The Answer is correct");
+  newP.appendChild(newContent);
+  const currentP = document.getElementById("answer");
+  document.body.insertBefore(newP, currentP);
+};
+
+const AnswerNo = document.querySelector(".no");
+AnswerNo.onclick = function () {
+  const newP = document.createElement("p");
+  const newContent = document.createTextNode("The Answer is wrong");
+  newP.appendChild(newContent);
+  const currentP = document.getElementById("answer");
+  document.body.insertBefore(newP, currentP);
 };
