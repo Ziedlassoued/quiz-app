271ae78d169a7146951b5c2efd3e4a0948d34ba3
ziedlassoued <zied.lassoued@outlook.de>
2021-10-12 14:21:31 +0200

    Add new p element in main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const correctAnswer = true;
-
-const myHeading = document.querySelector("h1");
+/*const myHeading = document.querySelector("h1");
 myHeading.textContent = "are you a robot?";
 
 const myQuestion = document.querySelector("p");
-myQuestion.textContent = "question two";
+myQuestion.textContent = "question two";*/
 
 /*document.querySelector(".yes").onclick = function () {
   if (correctAnswer === true) {
@@ -20,8 +19,39 @@ document.querySelector(".no").onclick = function () {
     alert("Correct");
   }*/
 
-const AnswerYes = document.querySelector(".yes");
-AnswerYes.onclick = function () {
+const yesButton = document.querySelector(".yes");
+yesButton.onclick = function () {
+  if (correctAnswer === true) {
+    showAnswerIsCorrect();
+  } else {
+    const resultElement = document.createElement("p");
+    resultElement.textContent = "that is not correct";
+    resultElement.className = "incorrect";
+    document.body.append(resultElement);
+  }
+};
+const noButton = document.querySelector(".no");
+noButton.onclick = function () {
+  if (correctAnswer === false) {
+    const resultElement = document.createElement("p");
+    resultElement.textContent = "It is correct";
+    resultElement.className = "correct";
+    document.body.append(resultElement);
+  } else {
+    const resultElement = document.createElement("p");
+    resultElement.textContent = "that is not correct";
+    resultElement.className = "incorrect";
+    document.body.append(resultElement);
+  }
+};
+function showAnswerIsCorrect() {
+  const resultElement = document.createElement("p");
+  resultElement.textContent = "It is correct";
+  resultElement.className = "correct";
+  document.body.append(resultElement);
+}
+
+/*}
   const newP = document.createElement("p");
   const newContent = document.createTextNode("The Answer is correct");
   newP.appendChild(newContent);
@@ -36,4 +66,4 @@ AnswerNo.onclick = function () {
   newP.appendChild(newContent);
   const currentP = document.getElementById("answer");
   document.body.insertBefore(newP, currentP);
-};
+};*/
